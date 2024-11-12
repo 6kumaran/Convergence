b9ca9a5d650ff0557b2aefd2799f22807fe57ca1
6kumaran <kumaran61003@gmail.com>
2024-11-12 20:14:27 +0530

    Modified Inaugral

--- src/app/Components/EventGallery/Gallery.tsx
+++ src/app/Components/EventGallery/Gallery.tsx
@@ -2,13 +2,13 @@
 import React from "react";
 import CaseStudyCard from "../animata/card/case-study-card";
 
-export default function BackgroundBeamsDemo() {
+const BackgroundBeamsDemo: React.FC = () => {
   return (
-    <div className="min-h-screen w-full bg-black flex flex-col items-center justify-center antialiased">
+    <div className="min-h-[80vh] w-full bg-black flex flex-col items-center justify-center antialiased">
       <div className="container mx-auto px-4 flex flex-wrap gap-4 justify-center">
         <CaseStudyCard
           category="GALLERY"
-          image="https://images.unsplash.com/photo-1675285410608-ddd6bb430b19?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          image="https://i.ibb.co/72BJ7zJ/backgrounds-savage-vinyl-backgrounds-sa-v20-01.jpg"
           link="https://drive.google.com/drive/folders/13sIOfgNlZ9lXZbewdT25lCZnksl80JHv?usp=sharing"
           logo="https://i.ibb.co/kHXdSrN/image.png"
           title="Inaugural Ceremony"
@@ -17,7 +17,7 @@ export default function BackgroundBeamsDemo() {
         />
         {/* <CaseStudyCard
           category="GALLERY"
-          image="https://images.unsplash.com/photo-1675285410608-ddd6bb430b19?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          image="https://i.ibb.co/72BJ7zJ/backgrounds-savage-vinyl-backgrounds-sa-v20-01.jpg"
           link="https://drive.google.com/drive/folders/13sIOfgNlZ9lXZbewdT25lCZnksl80JHv?usp=sharing"
           logo="https://i.ibb.co/kHXdSrN/image.png"
           title="Inaugural Ceremony"
@@ -26,7 +26,7 @@ export default function BackgroundBeamsDemo() {
         />
         <CaseStudyCard
           category="GALLERY"
-          image="https://images.unsplash.com/photo-1675285410608-ddd6bb430b19?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          image="https://i.ibb.co/72BJ7zJ/backgrounds-savage-vinyl-backgrounds-sa-v20-01.jpg"
           link="https://drive.google.com/drive/folders/13sIOfgNlZ9lXZbewdT25lCZnksl80JHv?usp=sharing"
           logo="https://i.ibb.co/kHXdSrN/image.png"
           title="Inaugural Ceremony"
@@ -35,14 +35,18 @@ export default function BackgroundBeamsDemo() {
         />
         <CaseStudyCard
           category="GALLERY"
-          image="https://images.unsplash.com/photo-1675285410608-ddd6bb430b19?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          image="https://i.ibb.co/72BJ7zJ/backgrounds-savage-vinyl-backgrounds-sa-v20-01.jpg"
           link="https://drive.google.com/drive/folders/13sIOfgNlZ9lXZbewdT25lCZnksl80JHv?usp=sharing"
           logo="https://i.ibb.co/kHXdSrN/image.png"
           title="Inaugural Ceremony"
           type="content"
           className="w-full sm:w-[calc(50%-1rem)] md:w-[calc(33.33%-1rem)] lg:w-[calc(25%-1rem)]"
         /> */}
+
       </div>
     </div>
   );
-}
+};
+
+BackgroundBeamsDemo.displayName = "BackgroundBeamsDemo";
+export default BackgroundBeamsDemo;
--- src/app/Components/animata/card/case-study-card.tsx
+++ src/app/Components/animata/card/case-study-card.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { cn } from "@/app/lib/utils";
 
 interface CaseStudyCardProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -8,7 +7,7 @@ interface CaseStudyCardProps extends React.HTMLAttributes<HTMLDivElement> {
   image?: string;
   logo?: string;
   link?: string;
-  type?: "content" | "simple-image"; // Decides between text or image
+  type?: "content" | "simple-image";
 }
 
 // ContentCard Component for rendering text + image
@@ -22,20 +21,17 @@ const ContentCard: React.FC<CaseStudyCardProps> = ({ title, category, image, log
         backgroundPosition: "center",
       }}
     >
-      {image && <div className="opacity-70rounded-lg absolute inset-0 bg-black" />}
+      {image && <div className="absolute inset-0 bg-black opacity-70 rounded-lg" />}
 
       <div className="relative z-10">
         {category && <div className="text-xs text-gray-200">{category}</div>}
-
         {title && (
           <div className="mr-2 text-lg font-bold leading-tight tracking-wide text-red-300">
             {title}
           </div>
         )}
       </div>
-      {logo && ( // Check if image exists
-        <img src={logo} alt={title} className="z-10 h-9 rounded-lg" />
-      )}
+      {logo && <img src={logo} alt={title} className="z-10 h-9 rounded-lg" />}
     </div>
   );
 };
@@ -54,25 +50,21 @@ const SimpleImageCard: React.FC<CaseStudyCardProps> = ({ image }) => {
   );
 };
 
-const HoverRevealSlip = ({ show }: { show: React.ReactNode }) => {
+const HoverRevealSlip: React.FC<{ show: React.ReactNode }> = ({ show }) => {
   const common = "absolute flex w-full h-full [backface-visibility:hidden]";
 
   return (
     <div className={cn("group relative h-60 w-52 [perspective:1000px]")}>
-      {/* Back cover - static */}
       <div className={cn("absolute inset-0 h-full w-48 rounded-lg bg-gray-50 shadow-md")}></div>
 
-      {/* Card container with slight book opening effect on hover */}
       <div
         className={cn(
           "relative z-50 h-full w-48 origin-left transition-transform duration-500 ease-out [transform-style:preserve-3d] group-hover:[transform:rotateY(-30deg)]",
         )}
       >
-        {/* Front side of the card */}
         <div className={cn("h-full w-full rounded-lg bg-white shadow-md", common)}>{show}</div>
       </div>
 
-      {/* Sliding link/tab coming out from behind */}
       <div
         className={cn(
           "z-1 absolute bottom-0 right-0 flex h-48 w-14 -translate-x-10 transform items-start justify-start rounded-r-lg bg-green-600 pl-2 pt-2 text-xs font-bold text-white transition-transform duration-300 ease-in-out [backface-visibility:hidden] group-hover:translate-x-0 group-hover:rotate-[5deg]",
@@ -84,15 +76,14 @@ const HoverRevealSlip = ({ show }: { show: React.ReactNode }) => {
   );
 };
 
-// Main CaseStudyCard Component
-export default function CaseStudyCard({
+const CaseStudyCard: React.FC<CaseStudyCardProps> = ({
   title,
   category,
   link,
   image,
   logo,
   type,
-}: CaseStudyCardProps) {
+}) => {
   return (
     <div className="flex gap-8">
       <a href={link} className="block">
@@ -101,11 +92,14 @@ export default function CaseStudyCard({
             type === "content" ? (
               <ContentCard title={title} category={category} image={image} logo={logo} />
             ) : (
-              <SimpleImageCard image={image} title={title} />
+              <SimpleImageCard image={image} />
             )
           }
         />
       </a>
     </div>
   );
-}
+};
+
+CaseStudyCard.displayName = "CaseStudyCard";
+export default CaseStudyCard;
